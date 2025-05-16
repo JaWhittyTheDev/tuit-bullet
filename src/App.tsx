148fc9b94df4420dc561c5915740b338c65145c6
JaWhittyTheDev <mironxxyt@gmail.com>
2025-05-16 15:10:57 +0200

    a

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    document.title = 'TUIT Bullet';
+  }, []);
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
