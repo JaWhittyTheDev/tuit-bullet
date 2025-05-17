8ab8b8554c6f6e80fc65b156e92ebda6edd45b40
JaWhittyTheDev <mironxxyt@gmail.com>
2025-05-17 09:48:05 +0200

    Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -9,8 +8,20 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      
+    <div id='app'>
+      <div className='flex items-center gap-4 mt-[20px] ml-[20px] mr-[20px]'>
+        <img src="https://picsum.photos/1000" alt="Logo" className='w-[100px] h-[100px] rounded-[50px]'/>
+        <p className='text-[#373C74] text-[25px] font-bold font-[Segoe_UI]'>
+          Bulletin of TUIT: Management and <br /> Communication Technologies
+        </p>
+
+        <div className='ml-auto flex gap-4'>
+          <a href="/send-paper" className='text-[#373C74] text-[20px] font-bold font-[Segoe_UI]'>SEND PAPER</a>
+          <a href="/sign" className='text-[#373C74] text-[20px] font-bold font-[Segoe_UI]'>SIGN IN / SIGN OUT</a>
+        </div>
+      </div>
+
+      <div className='w-[100%] h-[3px] bg-[#D2CDCD] mt-[20px]'></div>
     </div>
   );
 }
